b85c92fadc36627d2f8a2ee80d8202e434b7ba67
Fionan-Oxford <116331041+Fionan-Oxford@users.noreply.github.com>
2025-09-06 15:09:38 +0100

    Additional Path Planning Problem
    Additional Path Planning Problem

---
+++ problems/minimum_cost_to_make_one_valid_path.py
@@ -0,0 +1,100 @@
+"""Given an m x n grid.
+Each cell of the grid has a sign pointing to the next cell
+you should visit if you are currently in this cell.
+The sign of grid[i][j] can be:
+
+1 which means go to the cell to the right. (i.e go from grid[i][j] to grid[i][j + 1])
+2 which means go to the cell to the left. (i.e go from grid[i][j] to grid[i][j - 1])
+3 which means go to the lower cell. (i.e go from grid[i][j] to grid[i + 1][j])
+4 which means go to the upper cell. (i.e go from grid[i][j] to grid[i - 1][j])
+
+Notice that there could be some signs on the cells of the grid that point outside the grid.
+
+You will initially start at the upper left cell (0, 0).
+A valid path in the grid is a path that starts from the upper left cell (0, 0)
+and ends at the bottom-right cell (m - 1, n - 1) following the signs on the grid.
+The valid path does not have to be the shortest.
+You can modify the sign on a cell with cost = 1. You can modify the sign on a cell one time only.
+
+Return the minimum cost to make the grid have at least one valid path.
+Example 1:
+
+Input: grid = [[1,1,1,1],[2,2,2,2],[1,1,1,1],[2,2,2,2]]
+Output: 3
+Explanation: You will start at point (0, 0).
+The path to (3, 3) is as follows. (0, 0) --> (0, 1) --> (0, 2) --> (0, 3) change the arrow to down with cost = 1 --> (1, 3) --> (1, 2) --> (1, 1) --> (1, 0) change the arrow to down with cost = 1 --> (2, 0) --> (2, 1) --> (2, 2) --> (2, 3) change the arrow to down with cost = 1 --> (3, 3)
+The total cost = 3.
+Example 2:
+
+Input: grid = [[1,1,3],[3,2,2],[1,1,4]]
+Output: 0
+Explanation: You can follow the path from (0, 0) to (2, 2).
+Example 3:
+
+Input: grid = [[1,2],[4,3]]
+Output: 1
+"""
+
+from typing import List
+import heapq
+
+# Some thoughts.
+# At any stage of the grid, i should desire to go lower or further to right
+# Although there are rare occasions where I'd want to go further to the top or right (spiral route)
+# I'm thinking this is an a-star problem. Encourage going lower and to the right
+# Allow
+
+
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:
+
+        start = (0, 0)
+        m, n = len(grid), len(grid[0])
+        dest = (m - 1, n - 1)
+
+        if start == dest:
+            return 0
+
+        distance: dict[tuple[int, int], int] = {}
+        heap: list[tuple[int, tuple[int, int]]] = []
+        heapq.heappush(heap, (0, start))
+
+        # 1 which means go to the cell to the right. (i.e go from grid[i][j] to grid[i][j + 1])
+        # 2 which means go to the cell to the left. (i.e go from grid[i][j] to grid[i][j - 1])
+        # 3 which means go to the lower cell. (i.e go from grid[i][j] to grid[i + 1][j])
+        # 4 which means go to the upper cell. (i.e go from grid[i][j] to grid[i - 1][j])
+
+        directions = {1: (0, 1), 2: (0, -1), 3: (1, 0), 4: (-1, 0)}
+
+        while heap:
+            distance_c, (cr, cc) = heapq.heappop(heap)
+
+            direction_c = grid[cr][cc]
+
+            for dr, dc in directions.values():
+                nr, nc = cr + dr, cc + dc
+                if 0 <= nr < m and 0 <= nc < n:  # We are on the grid
+                    if directions[direction_c] == (dr, dc):
+                        cost = 0
+                    else:
+                        cost = 1
+
+                    distance_n = distance_c + cost
+
+                    if (nr, nc) in distance:
+                        if distance[(nr, nc)] <= distance_n:
+                            continue
+
+                    distance[(nr, nc)] = distance_n
+                    heapq.heappush(heap, (distance_n, (nr, nc)))
+
+        if dest in distance:
+            return distance[dest]
+        else:
+            return -1
+
+
+mySolution = Solution()
+input = [[1, 1, 1, 1], [2, 2, 2, 2], [1, 1, 1, 1], [2, 2, 2, 2]]
+
+print(mySolution.minCost(input))
